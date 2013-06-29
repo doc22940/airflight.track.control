6bdc360b858403ffe86c5a043e34093915579ad5
Andrey Lebedev <andrey@lebedev.lt>
2013-06-30 00:24:15 +0300

    Unlock achievement after detailed flight analysis

--- skylines/worker/tasks.py
+++ skylines/worker/tasks.py
@@ -4,6 +4,7 @@ from celery.utils.log import get_task_logger
 from skylines.lib.xcsoar_ import analysis
 from skylines.worker.celery import celery
 from skylines.model import db, Flight
+from skylines.model.achievement import unlock_flight_achievements
 
 logger = get_task_logger(__name__)
 
@@ -12,7 +13,13 @@ logger = get_task_logger(__name__)
 def analyse_flight(flight_id, full=2048, triangle=6144, sprint=512):
     logger.info("Analysing flight %d" % flight_id)
 
-    if analysis.analyse_flight(Flight.get(flight_id), full, triangle, sprint):
-        db.session.commit()
-    else:
+    flight = Flight.get(flight_id)
+    success = analysis.analyse_flight(flight, full, triangle, sprint)
+
+    if not success:
         logger.warn("Analysis of flight %d failed." % flight_id)
+        return
+
+    unlock_flight_achievements(flight)
+
+    db.session.commit()
